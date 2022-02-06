4372de7882b3ba20279854ce76d2e8e3e94775ee
Dauren Makulbekov <dauren_d.m@mail.ru>
2022-02-06 12:31:11 +0600

    Improved design

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -47,6 +47,6 @@ class ProjectsController < ApplicationController
 
   private
     def project_params
-      params.require(:project).permit(:name, :user_id)
+      params.require(:project).permit(:name, :user_id, :body, :file, images: [])
     end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
-
     belongs_to :user
+    has_rich_text :body
+    has_many_attached :images
+    has_one_attached :file
 end
